1d6cb5b54cf463511d33f856f681af6da4620583
dasaniket20002 <dasaniket20002@gmail.com>
2024-02-12 15:59:07 +0530

    first design commit

--- weatherapp/src/App.tsx
+++ weatherapp/src/App.tsx
@@ -1,22 +1,25 @@
 import React, { useState } from 'react';
 import { InputText } from 'primereact/inputtext';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import { RegisterPage } from './components/RegisterPage';
+ 
 const api = {
     key: "195887370f9dafce79b410c97c2925f5",
     base: "https://api.openweathermap.org/data/2.5/",
 }
-
+ 
 function App() {
-  return (
-    <div className="App min-h-screen">
-        <BrowserRouter>
-            <Routes>
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/" element={<HomePage />} />
-            </Routes>
-        </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/" element={<HomePage />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
-
+ 
 export default App;
